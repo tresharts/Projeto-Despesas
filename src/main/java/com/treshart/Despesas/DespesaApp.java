a984ce12b3023b4941fa5080943565bf26515be8
tresharts <arthurbruno489@gmail.com>
2025-09-15 18:13:22 -0300

    Application

---
+++ src/main/java/com/treshart/Despesas/DespesaApp.java
@@ -0,0 +1,13 @@
+package com.treshart.Despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesaApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DespesaApp.class, args);
+    }
+}
